16ea94922ffd0cee8bf2649bfa8a6bd30202db08
Ajay S <ajaysankar2502@gmail.com>
2023-06-20 14:27:42 +0530

    commit

--- MotorcycleService/src/main/java/com/example/demo/Repository/Repo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.Model.Pojo;
-
-public interface Repo extends JpaRepository<Pojo, String>
-{
-	
-}
--- MotorcycleService/src/main/java/com/example/demo/Repository/RepoSpares.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.Model.PojoSpares;
-
-public interface RepoSpares extends JpaRepository<PojoSpares, Integer>{
-
-}
